381cc259fb1ffaa1fc821de4ae5d8306b3b3adb6
Astorix10 <alexdoesnt@gmail.com>
2022-12-04 18:27:35 +0100

    staff bozza

---
+++ src/main/java/it/unical/demacs/progetto/cbeat/cbeat/controller/HomepageStaffController.java
@@ -0,0 +1,29 @@
+package it.unical.demacs.progetto.cbeat.cbeat.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class HomepageStaffController {
+
+    @FXML
+    private Button LogoutButton;
+
+    @FXML
+    private VBox OrderList;
+
+    @FXML
+    private Label WelcomeLabel;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    void DoLogout(ActionEvent event) {
+
+    }
+
+}
